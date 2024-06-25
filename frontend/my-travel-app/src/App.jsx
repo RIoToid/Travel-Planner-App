20a9c7ef86f9b0effa66d4edfbd9ebc8bc263be7
RIoToid <jana.elhelou@gmail.com>
2024-06-25 11:17:56 +0300

    refactor: migrate from create-react-app to vite

--- frontend/my-travel-app/src/App.jsx
+++ frontend/my-travel-app/src/App.jsx
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
   const [data, setData] = useState([]);
